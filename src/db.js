bfc79b243dd84e0a7cf311652334ab5da9940863
igorunderplayer <igorschug6@gmail.com>
2022-01-11 19:14:01 -0300

    update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ const SimplDb = require('simpl.db')
 
 const db = SimplDb({
   collectionsFolder: './collections',
-  dataFile: './database/db.json'
+  dataFile: './src/database/db.json',
+  autoSave: true
 })
 
 const Guilds = db.createCollection('guilds')
